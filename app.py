82f69bc5dd89416dbc611259dba08b6046c07fac
Dan Thompson <danielt397@gmail.com>
2020-03-30 14:07:16 +0100

    soon

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-import gui
+import old
 from tkinter.filedialog import askopenfilename
 
 if __name__ == '__main__':
     audio_path = askopenfilename()
     excel_file = 'Genre.xlsx'
-    gui.main(audio_path, excel_file)
+    old.main(audio_path, excel_file)
