faf0321d70e7b293a0040449cab5e698558d5882
Dan Thompson <zcemdjt@ucl.ac.uk>
2020-10-10 20:14:14 +0100

    Alpha 0.3.5 - Backup

---
+++ player_vlc_backup.py
@@ -0,0 +1,23 @@
+import vlc
+
+
+def play(path):
+    instance = vlc.Instance()
+
+    # Create a MediaPlayer with the default instance
+    play.player = instance.media_player_new()
+
+    # Load the media file
+    media = instance.media_new(path)
+
+    # Add the media to the player
+    play.player.set_media(media)
+    play.player.audio_set_volume(65)
+
+    # Play for 10 seconds then exit
+    play.player.play()
+    play.player.set_position(0.3)
+
+
+def stop(path):
+    play.player.stop()
